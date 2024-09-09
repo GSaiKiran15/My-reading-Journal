2d5801477469872eac03bfd9862c18c4567f5ccb
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2024-09-08 21:43:29 -0600

    day 2 changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import axios from "axios";
-// import { database, host, password, port } from "pg/lib/defaults";
 
 const PORT = 3000;
 const app = express();
@@ -22,9 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   const { rows: book } = await db.query("select * from books");
-  console.log(book[0].s_desc);
-
+  console.log(book[0]);
   res.render("index.ejs", { book });
+});
+
+app.post("/full", async (req, res) => {
+  let selectedBookID = parseInt(req.body.clickID);
+  console.log(typeof selectedBookID, selectedBookID);
+  const { rows: book } = await db.query("select * from books where id = $1", [
+    selectedBookID,
+  ]);
+  res.render("description.ejs", {
+    book: book[0],
+  });
 });
 
 app.listen(PORT, () => {
